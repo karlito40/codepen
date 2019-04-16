d682acd6485d4be70177979b2a0a76f7bbcd2cd5
karlito40 <karlito40@gmail.com>
2019-04-16 21:55:43 +0200

    init grid builder

---
+++ grid-builder/src/components/Toolbox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="Toolbox">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis quaerat at similique fuga? Error quae aperiam laborum a officia ratione dolorum! Ea doloribus necessitatibus architecto explicabo velit fuga harum aliquam.</p>
+    <p>Magnam minima iste perferendis impedit molestias maxime fugiat placeat eius reiciendis quia. Cum voluptas adipisci consequuntur asperiores voluptatum beatae excepturi tempore mollitia delectus necessitatibus repellat et cumque, temporibus qui libero!</p>
+    <p>Eaque, at. Suscipit error nobis doloremque quaerat, amet quasi voluptas accusantium pariatur temporibus illum maiores, a dicta voluptatibus reprehenderit reiciendis excepturi, incidunt at quidem blanditiis libero eum perspiciatis tempore animi.</p>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.Toolbox {
+  @extend .box;
+  height: 200px;
+  width: 100px;
+}
+</style>
---
+++ grid-builder/src/test-grid/App.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="app">
+		<h1>Display grid</h1>
+		
+		<section class="site">
+			<header class="header">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam explicabo aut praesentium et illo at sequi! Labore veritatis tempora dignissimos quo et a explicabo ipsam aliquid, mollitia, cum voluptates quae!
+			</header>
+			<article class="body">
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque nisi aliquam quae eos, magnam molestias vero quis, libero quisquam earum quos tempora nam at recusandae saepe laudantium voluptates maxime enim.</p>
+				<p>Libero quas distinctio nisi reiciendis vel totam natus delectus voluptatem pariatur voluptas cupiditate, laudantium perspiciatis nesciunt molestiae aliquam laborum repellat sunt aliquid rem? Molestiae optio tempora soluta, ratione quos libero.</p>
+				<p>Veniam mollitia quisquam accusantium quibusdam doloribus cumque facilis repudiandae possimus quod quas accusamus impedit, blanditiis voluptatum, libero laboriosam inventore. Corporis minima sint quod porro officia eligendi molestias veritatis, laborum quia.</p>
+			</article>
+			<aside class="sidebar">
+				<ul>
+					<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est sint ipsam alias autem nihil deserunt, culpa illum, nam modi fugit molestiae! Placeat numquam vitae sunt optio architecto dolorum, soluta nam?</li>
+					<li>Harum accusamus sint beatae enim, commodi vitae modi eos cupiditate pariatur itaque laboriosam est ipsam eius tenetur nobis qui doloremque dolores voluptatum ad earum ut. Non porro autem consequatur consectetur.</li>
+					<li>Perferendis commodi quo veritatis dicta maxime voluptate, delectus laborum odit sequi repellat neque cum possimus nihil dolores voluptatem aperiam qui iusto laudantium expedita illum, assumenda tenetur cupiditate incidunt. Est, alias!</li>
+					<li>Eius dolorum totam alias eligendi assumenda eaque repellendus reiciendis id fugiat? Fugiat, incidunt! Voluptas maiores saepe commodi iste vel doloribus nemo molestiae beatae vero, magni numquam alias, officia molestias? Similique.</li>
+				</ul>
+			</aside>
+			<footer class="footer">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro iure ipsam repellat quisquam amet quidem repudiandae aliquam necessitatibus, consectetur ipsum magni delectus unde? Ex vero incidunt earum illo pariatur. Odit.
+			</footer>
+		</section>
+	</div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style lang="scss">
+html, body {
+	margin: 0;
+	padding: 0;
+}
+</style>
+
+<style lang="scss" scoped>
+@import './app.scss';
+</style>
