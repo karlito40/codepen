d941c1345a23d7a54c0db761afb3b710ef4b275b
karlito40 <karlito40@gmail.com>
2019-06-17 21:18:42 +0200

    init smartpad

---
+++ smartpad/smartpad-front/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span class="font-weight-light">Smart</span>
+        <span>PAD</span>
+      </v-toolbar-title>
+    </v-toolbar>
+
+    <v-content>
+      <Home/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Home from './views/Home'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
