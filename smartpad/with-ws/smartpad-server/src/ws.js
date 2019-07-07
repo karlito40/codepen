e9ede102f847fb4769e9c7d4222466305a0403cb
karlito40 <karlito40@gmail.com>
2019-07-07 16:43:49 +0200

    fix: window drag

--- smartpad/with-ws/smartpad-server/src/ws.js
+++ smartpad/with-ws/smartpad-server/src/ws.js
@@ -6,17 +6,25 @@ const wss = new WebSocket.Server({ port: 8081 })
 
 robot.setMouseDelay(0);
 
-let mouse = robot.getMousePos();
+let mouse = { 
+  ...robot.getMousePos(),
+  isDragging: false
+};
 let prevMouse = mouse;
 
 function updateMouse(x, y) {
   prevMouse = { ...mouse };
-  mouse = { x, y };
+  mouse = { x, y, isDragging: mouse.isDragging };
 }
 
 (function loop() {
   if(!isEqual(mouse, prevMouse)) {
-    robot.moveMouse(mouse.x, mouse.y);
+    if(mouse.isDragging) {
+      robot.dragMouse(mouse.x, mouse.y);
+    } else {
+      robot.moveMouse(mouse.x, mouse.y);
+    }
+    
     prevMouse = mouse;
   }
   
@@ -29,12 +37,14 @@ wss.on('connection', ws => {
     console.log(subject, data);
 
     if(subject === 'mouse:down') {
+      mouse.isDragging = true;
       robot.mouseToggle('down');
       ws.send(JSON.stringify({
         subject: 'mouse:down',
         data: { enabled: true }
       }));
     } else if(subject === 'mouse:up') {
+      mouse.isDragging = false;
       robot.mouseToggle('up');
     } else if(subject === 'mouse:move') {
       const destX = mouse.x + data.x;
