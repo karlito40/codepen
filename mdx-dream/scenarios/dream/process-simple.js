ed49c091e76896856341345013e4a81562404da2
karlito40 <karlito40@gmail.com>
2020-02-13 23:51:02 +0100

    feat(mdx-dream): operation by collection

--- mdx-dream/scenarios/dream/process-simple.js
+++ mdx-dream/scenarios/dream/process-simple.js
@@ -70,7 +70,7 @@ export default async function Snapshot (collection, where, initialState) {
   snapshot.onUpdate = (cb) => event.on('updated', cb);
 
   // where filter on listener 
-  Pubsub.on(`${collection}.snapshot.updated`, where, (changes) => {
+  Pubsub.on(`${collection}.snapshot.updated`, FilterBy(where), ({ changes }) => {
     snapshot.state = deepAssign(snapshot.state, changes);
     event.emit('updated', changes);
   });
@@ -85,7 +85,7 @@ const $db = {
     return mongodb.__operations.insert({ operation });
   },
 
-  addSnapshot (collection) {
+  async addSnapshot (collection) {
     const lastSnapshot = await mongodb[collection].find({}).sort({ _id: -1 }).limit(1);
     const events = mongodb.__operations.find({ createdAt: { $lt: lastSnapshot?.createdAt } });
     
@@ -93,14 +93,20 @@ const $db = {
 
     let state = lastSnapshot ? lastSnapshot.state : events[0].state;
     events.forEach((event) => {
+      // get operation that only affect this collection
+      const operation = getOperationFromCollection(collection, event.operation);
       // parse operations and resolve a new state from them
-      state = driver(state, event.operation);
+      state = driver(state, operation);
     });
   
     await mongodb[collection].insert({ state });
 
     if (lastSnapshot) {
-      Pubsub.emit(`${collection}.snapshot.updated`, 'TODO', diff(lastSnapshot.state, state));
+      // TODO: handle where filter without sending all snapshot
+      Pubsub.emit(`${collection}.snapshot.updated`, {
+        where: associate(Object.keys(await mongodb[collection].getIndexes(), state)), // well just to show the logic behind...
+        changes: diff(lastSnapshot.state, state)
+      });
     }
   },
 
@@ -116,6 +122,9 @@ const $db = {
   },
 
   tick () {
-    this.addSnapshot();
+    const collectionNames = await mongodb.getCollectionNames();
+    collectionNames.forEach((collection) => {
+      this.addSnapshot(collection);
+    });
   }
 }
